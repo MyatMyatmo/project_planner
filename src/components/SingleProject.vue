016d873c6fb64f93bc5dc980e761ffad565b9daa
NanPanWinPhyu <nanpanwinphyu@gmail.com>
2021-11-22 11:09:00 +0630

    add_css_and_icons_2021_11_22

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="project" @click="showDetail=!showDetail">
-        <h3>{{project.title}}</h3>
+    <div class="project">
+        <div class="flexing">
+            <div>
+                <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+            </div>
+            <div>
+                <span class="material-icons">
+                delete
+                </span>
+                <span class="material-icons">
+                edit
+                </span>
+                <span class="material-icons">
+                done
+                </span>
+            </div>
+        </div>
         <p v-if="showDetail">{{project.detail}}</p>
     </div>
 </template>
@@ -20,11 +35,24 @@ export default {
 .project {
     padding:20px;
     background-color: #f2f2f2;
-    cursor: pointer;
-    margin: 10px
+    margin: 10px;
+    border-left: 6px solid crimson;
+    border-radius: 8px;
 }
 h3 {
     color:indigo;
+    cursor: pointer;
 }
-
+.flexing {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+span {
+    margin-left: 6px;
+    cursor: pointer;
+}
+span:hover {
+    color: #777;
+}
 </style>
